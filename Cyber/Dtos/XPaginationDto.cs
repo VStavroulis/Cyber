38c57897e2e6bfc4e1404388c5e0ef5a5c45f83d
VStavroulis <vagio4@hotmail.com>
2022-09-25 22:55:19 +0300

    .

---
+++ Cyber/Dtos/XPaginationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cyber.Dtos
+{
+    public class XPaginationDto
+    { 
+        public int total_record_count { get; set; }
+        public int page_size { get; set; }
+        public int page_no { get; set; }
+        public int page_count { get; set; }
+    }
+}
--- Cyber/Services/AlertService.cs
+++ Cyber/Services/AlertService.cs
@@ -16,18 +16,42 @@ namespace Cyber.Services
 
         public async Task<List<AlertDto>> GetAlerts()
         {
+            
             _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
 
             using (var httpClient = new HttpClient(_clientHandler))
             {
-                using (var response = await httpClient.GetAsync("https://localhost:5001/api/alerts/a?page_size=3&page_no=2"))
+                int pageSize = 100;
+                int pageNo = 1;
+                List<AlertDto> alertDtos = new List<AlertDto>();
+                using (var response = await httpClient.GetAsync(string.Format("https://localhost:5001/api/alerts/a?page_size={0}&page_no={1}", pageSize, pageNo)))
                 {
+                    int currentPage = 1;
+                    
+                    var apiHeaders =  response.Headers.GetValues("X-Pagination").ToList().FirstOrDefault();
+                    var xPaginationDto = JsonConvert.DeserializeObject<XPaginationDto>(apiHeaders);
+                    
+                    var pageCount = xPaginationDto.total_record_count;
+
                     string apiResponse = await response.Content.ReadAsStringAsync();
-
+                    
                     var alertList = JsonConvert.DeserializeObject<List<AlertDto>>(apiResponse);
-
-                    return alertList;
+                    
+                    alertDtos.AddRange(alertList);
+                    while (pageNo <= xPaginationDto.page_count)
+                    {
+                        pageNo++;
+                        using (var nextResponse = await httpClient.GetAsync(string.Format("https://localhost:5001/api/alerts/a?page_size={0}&page_no={1}", pageSize, pageNo)))
+                        {
+                            string nextApiResponse = await nextResponse.Content.ReadAsStringAsync();
+                            var nextAlertList = JsonConvert.DeserializeObject<List<AlertDto>>(nextApiResponse);
+                            alertDtos.AddRange(nextAlertList);
+                        }
+                    }
+                    return alertDtos;
                 }
+
+                
             }
         }
     }
